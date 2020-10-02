805f9eeaf37d97eeeee9a36467f512d1a33c5479
IsuruHaupe <isuru.liyanage.20@gmail.com>
2020-10-02 14:42:06 +0200

    added csv part

--- cmd/project/main.go
+++ cmd/project/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"../../internal/paho"
@@ -11,11 +14,76 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+//teste si un fichier existe
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+//cree le fichier csv ou ajoute les donnees a la fin
+func generateDataLake(tab []string) {
+	//creer le nom du fichier : idAirport_timestamp_sensorType.csv
+	filename := strings.Split(tab[1], ":")[1] + "_" +
+		strings.Split(tab[4], "timestamp:")[1] + "_" +
+		strings.Split(tab[2], ":")[1] + ".csv"
+	//tester si le csv existe avec le nom du fichier
+	if fileExists(filename) {
+		//ouvrir le csv et ajouter les donnees
+		f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		//formattage des donnees avant ajout
+		var data [][]string
+		data = append(data, []string{strings.Split(tab[0], ":")[1], strings.Split(tab[3], ":")[1]})
+		//ecrire les donnees
+		w := csv.NewWriter(f)
+		w.WriteAll(data)
+
+		if err := w.Error(); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Appending succed")
+	} else {
+		//creer le fichier et ajouter les donnees
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatal("Impossible de créer le fichier", err)
+		}
+		defer file.Close()
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+		//formatter les donnees a ajouter
+		var data [][]string
+		data = append(data, []string{"idSensor", "Valeur"})
+		data = append(data, []string{strings.Split(tab[0], ":")[1], strings.Split(tab[3], ":")[1]})
+		for _, value := range data {
+			err := writer.Write(value)
+			if err != nil {
+				log.Fatal("Impossible d'écrire dans le fichier", err)
+			}
+		}
+		fmt.Println("Creation & Appending succed")
+	}
+}
+
+func generateCsvData(message []byte) {
+	//conversion en string pour pouvoir filtrer
+	res := string(message)
+	//split sur les retours charriots puis parsing avec la fonction
+	generateDataLake(strings.Split(res, "\n"))
+}
+
 func onMessageReceived(client mqtt.Client, message mqtt.Message) {
 	// process message here
 	// redis
-	// CSV
 	fmt.Printf("Received message on topic: %s\nMessage: %s\n", message.Topic(), message.Payload())
+	//partie csv
+	generateCsvData(message.Payload())
 }
 
 func main() {
--- internal/paho/pub.go
+++ internal/paho/pub.go
@@ -1,6 +1,9 @@
 package paho
 
 import (
+	"fmt"
+	"math/rand"
+	"strconv"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -24,26 +27,72 @@ func getNewData() {
 func startPubTemp(client mqtt.Client, topic string, period time.Duration) {
 	timer := time.NewTicker(period * time.Second)
 	for t := range timer.C {
-		client.Publish(topic, 0, false, "temp : "+t.String())
+		d1 := data{
+			idSensor:   01,
+			idAirport:  "CDG",
+			sensorType: "temperature",
+			value:      (-5) + rand.Float64()*(30-(-5)), //valeur a modifier
+			timestamp:  t,
+		}
+		//conversion en string sinon peut pas envoyer de message
+		text := fmt.Sprintf(
+			"idSensor:" + strconv.Itoa(d1.idSensor) + "\n" +
+				"idAirport:" + d1.idAirport + "\n" +
+				"sensorType:" + d1.sensorType + "\n" +
+				"value:" + strconv.FormatFloat(d1.value, 'f', -1, 64) + "\n" +
+				"timestamp:" + d1.timestamp.Format("2006-01-02")) //on doit donner un exemple de formattage pour qu'il comprend
+		client.Publish(topic, 0, false, text)
+		//client.Publish(topic, 0, false, d1)
 	}
 }
 
 func startPubWind(client mqtt.Client, topic string, period time.Duration) {
 	timer := time.NewTicker(period * time.Second)
 	for t := range timer.C {
-		client.Publish(topic, 0, false, "wind : "+t.String())
+		d1 := data{
+			idSensor:   01,
+			idAirport:  "JFK",
+			sensorType: "vent",
+			value:      (0) + rand.Float64()*(100-0), //valeur a modifier
+			timestamp:  t,
+		}
+		//conversion en string sinon peut pas envoyer de message
+		text := fmt.Sprintf(
+			"idSensor:" + strconv.Itoa(d1.idSensor) + "\n" +
+				"idAirport:" + d1.idAirport + "\n" +
+				"sensorType:" + d1.sensorType + "\n" +
+				"value:" + strconv.FormatFloat(d1.value, 'f', -1, 64) + "\n" +
+				"timestamp:" + d1.timestamp.Format("2006-01-02")) //on doit donner un exemple de formattage pour qu'il comprend
+		client.Publish(topic, 0, false, text)
+		//client.Publish(topic, 0, false, "wind : "+t.String())
 	}
 }
 
 func startPubPressure(client mqtt.Client, topic string, period time.Duration) {
 	timer := time.NewTicker(period * time.Second)
 	for t := range timer.C {
-		client.Publish(topic, 0, false, "pressure : "+t.String())
+		d1 := data{
+			idSensor:   01,
+			idAirport:  "JFK",
+			sensorType: "pression",
+			value:      (1) + rand.Float64()*(5-1), //valeur a modifier
+			timestamp:  t,
+		}
+		//conversion en string sinon peut pas envoyer de message
+		text := fmt.Sprintf(
+			"idSensor:" + strconv.Itoa(d1.idSensor) + "\n" +
+				"idAirport:" + d1.idAirport + "\n" +
+				"sensorType:" + d1.sensorType + "\n" +
+				"value:" + strconv.FormatFloat(d1.value, 'f', -1, 64) + "\n" +
+				"timestamp:" + d1.timestamp.Format("2006-01-02")) //on doit donner un exemple de formattage pour qu'il comprend
+		client.Publish(topic, 0, false, text)
+		//client.Publish(topic, 0, false, "pressure : "+t.String())
 	}
 }
 
 // StartSensorsPubs : start all three sensors
 func StartSensorsPubs(client mqtt.Client, topic string, period time.Duration) {
+	rand.Seed(time.Now().UnixNano())
 	go startPubTemp(client, topic, period)
 	go startPubWind(client, topic, period)
 	go startPubPressure(client, topic, period)

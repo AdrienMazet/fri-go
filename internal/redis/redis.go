8756bc1294359d45a0741f1add5d29e84528fc5f
Adrien Mazet <adrien.mazet.56@gmail.com>
2020-10-06 00:27:24 +0200

    write to redis

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strconv"
 
@@ -9,10 +10,10 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-// faire un singleton ?
-
 var ctx = context.Background()
 
+// TODO : Singleton ?
+// TODO : Configure redis (port, credentials, persistence) ?
 var rdb = redis.NewClient(&redis.Options{
 	Addr:     "localhost:6379",
 	Password: "", // no password set
@@ -20,8 +21,16 @@ var rdb = redis.NewClient(&redis.Options{
 })
 
 // StoreData : store sensor data in redis database
-func StoreData(data sensor.Data) {
-	key := strconv.Itoa(data.IDSensor) + ":" + data.IDAirport + ":" + data.SensorType + data.Timestamp.String()
+func StoreData(message []byte) {
+	var data sensor.Data
+	json.Unmarshal(message, &data)
+	writeData(data)
+}
+
+func writeData(data sensor.Data) {
+	ISODateLayout := "2006-01-02 15:04:05"
+	key := strconv.Itoa(data.IDSensor) + ":" + data.IDAirport + ":" + data.SensorType + ":" + data.Timestamp.Format(ISODateLayout)
+
 	err := rdb.Set(ctx, key, data.Value, 0).Err()
 	if err != nil {
 		panic(err)
